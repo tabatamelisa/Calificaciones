50930b8faf9a58f5117602dfd722c8e05b274808
tabatamelisa <tabatavernaza12@gmail.com>
2025-03-13 13:26:09 -0500

    primer commit

---
+++ Calificaciones/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calificaciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Validar la nota final de un estudiante
+            //Andy Garzón
+            int calificacion;
+            do
+            {
+                Console.Write("Ingresa tu calificación en números (0-100): ");
+                calificacion = int.Parse(Console.ReadLine());
+
+                if (calificacion >= 90)
+                {
+                    Console.WriteLine("Tu calificación es A");
+                }
+                else if (calificacion >= 80)
+                {
+                    Console.WriteLine("Tu calificación es B");
+                }
+                else if (calificacion >= 70)
+                {
+                    Console.WriteLine("Tu calificación es C");
+                }
+                else if (calificacion >= 60)
+                {
+                    Console.WriteLine("Tu calificación es D");
+                }
+                else if (calificacion <= 59)
+                {
+                    Console.WriteLine("Tu calificación es F");
+                }
+            } while (true); // Se repite el programa
+        }
+    }
+}
